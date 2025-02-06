f4c9f8544a596f3f7bcbddced0e9a223673e63af
Mahdi T <78121487+MahdiT54@users.noreply.github.com>
2025-02-06 18:46:23 -0500

    new items countdown in progress

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,11 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import nftImage from "../../images/nftImage.jpg";
+// import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 
 const NewItems = () => {
   const [items, setItems] = useState([]);
 
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatTime = (totalSeconds) => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    const formattedHours = String(hours).padStart(1, "0");
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const formattedSeconds = String(seconds).padStart(2, "0");
+
+    return `${formattedHours}h ${formattedMinutes}m ${formattedSeconds}s`;
+  };
   useEffect(() => {
     axios
       .get(
@@ -27,7 +47,11 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {items.map((item, index) => (
+          {items.map((item, index) => {
+            const expiryTime = new Date(item.expiryDate);
+            let timeLeftInSeconds = Math.floor((expiryTime - currentTime) / 1000);
+            if (timeLeftInSeconds < 0) timeLeftInSeconds = 0;
+            return (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
@@ -41,7 +65,7 @@ const NewItems = () => {
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
+                <div className="de_countdown">{formatTime(timeLeftInSeconds)}</div>
 
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
@@ -82,7 +106,8 @@ const NewItems = () => {
                 </div>
               </div>
             </div>
-          ))}
+          );
+        })}
         </div>
       </div>
     </section>
